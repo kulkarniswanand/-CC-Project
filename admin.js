1f24dbc2baf582ead71827fa5abe809972db75a7
Swanand Kulkarni <138847934+kulkarniswanand@users.noreply.github.com>
2023-12-03 19:14:44 +0530

    js changes

--- admin.js
+++ admin.js
@@ -1,5 +1,21 @@
 function order()
 {
-    document.getElementById('tb').style.visibility='visible';
+    // document.getElementById('tb').style.visibility='visible';
     document.getElementById('ord').style.backgroundColor = "#fb8500";
+}
+
+// var ch=document.getElementById("check1");
+// ch.addEventListener("onclick", function(){
+//         alert("you have cliked");
+// });
+
+function checked(icon)
+{
+    var clikedRow=icon.closest('tr');
+    var pendingtable=document.getElementById("tb1");
+    var cloneRow=clikedRow.cloneNode(true);
+    pendingtable.appendChild(cloneRow);
+    clikedRow.parentNode.removeChild(clikedRow);
+
+
 }
